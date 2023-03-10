120946dac26ea67311c13d2661f4d3e951e95a03
Israel Salinas <israel.salinas.m@gmail.com>
2023-03-09 22:21:20 -0600

    ejercicio completo

--- s13eventListeners/js/main.js
+++ s13eventListeners/js/main.js
@@ -20,3 +20,25 @@ checkbox.forEach((element) => {
     console.log("cambiando");
   });
 });
+
+/*
+{
+    name:"some value",
+    lastname:"some value",
+    email:"some value"
+}
+*/
+
+let fields = document.querySelectorAll("#person-form input");
+let personObject = {};
+
+fields.forEach((field) => {
+  field.addEventListener("keyup", (event) => {
+    let property = event.target.name;
+    let value = event.target.value;
+    personObject[property] = value;
+    console.log(personObject);
+    document.getElementById("code-sample").innerText =
+      JSON.stringify(personObject);
+  });
+});
